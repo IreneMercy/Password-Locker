6b3315d04af463ce4c26209a13c8da9d22c4be8a
IreneMercy <irenemercy700@gmail.com>
2019-09-01 14:13:28 +0300

    create method to save credentials

--- password_locker.py
+++ password_locker.py
@@ -2,6 +2,7 @@ class Credentials:
     '''
     Class that generates ne password credentials
     '''
+    credential_list = []
     def __init__(self, first_name, last_name, email, password, username):
         '''
         __init__ metho that generate properties for our objects
@@ -11,3 +12,9 @@ class Credentials:
         self.email = email
         self.password = password
         self.username = username
+
+    def save_credentials(self):
+        '''
+        save creentials into the credential_list
+        '''
+        Credentials.credential_list.append(self)
