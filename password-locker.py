4bb5feb130863554c4d98755f80e709ece7625fe
IreneMercy <irenemercy700@gmail.com>
2019-09-01 13:07:20 +0300

    create instance of the class

--- password-locker.py
+++ password-locker.py
@@ -1,1 +1,13 @@
 class Password:
+    '''
+    Class that generates ne password credentials
+    '''
+    def __init__(self, first_name, last_name, email, password, username):
+        '''
+        __init__ metho that generate properties for our objects
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = Password
+        self.username = username
