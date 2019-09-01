d3f23f2c76ffe34410110e8c8648767aaf10ec41
IreneMercy <irenemercy700@gmail.com>
2019-09-01 14:58:28 +0300

    add method that takes a username and return credential that matches it

--- password_locker.py
+++ password_locker.py
@@ -15,12 +15,24 @@ class Credentials:
 
     def save_credentials(self):
         '''
+        method that saves a credential
         enables user to save creentials into the credential_list
         '''
         Credentials.credential_list.append(self)
 
     def delete_credential(self):
         '''
+        method that deletes a credential
         enable user to delete credential
         '''
         Credentials.credential_list.remove(self)
+
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        method that takes a username and return credential that matches it
+        enable user to search for a specific credential
+        '''
+        for credential in cls.credential_list:
+            if credential.username == username:
+                return credential
